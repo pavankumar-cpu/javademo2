c571c324f0c902d111dbf792115f6c88e5cff4b3
pavankumar-cpu <pavankumaramadakayala@gmail.com>
2024-09-13 12:57:41 +0530

    Add files via upload

---
+++ typefactorial.java
@@ -0,0 +1,10 @@
+class factorial
+{
+public static void main(string args[])}
+int i,fact=1;
+int number=5;//it is the number to calculate factorial
+for(i=1;i<=number;i++)
+{
+	fact=fact*i;
+}
+system.out.println("factorial of"+number+"is:"+fact);
